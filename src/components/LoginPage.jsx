90adbe4df744c07c24cc66b69a5a51004f1cc4d2
Rev-namdas <sadmanahmedsadman@gmail.com>
2022-04-21 17:18:14 +0600

    Layout Changed

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./login.css"
 import LoginImage from "../assets/img/login.png"
 import RyansMedia from "../assets/img/ryansmedia-logo.png"
@@ -8,43 +8,74 @@ import PasswordIcon from "../assets/img/password.png"
 export default function LoginPage() {
 	document.title = "Ryans Media - Login"
 
-  return (
-	<div className="page-bg">
-		<div className="login-container row">
-			<div className="col-md-7 design-wrapper">
-				<div className="img-wrapper">
-					<img className="img-responsive" src={LoginImage} alt="Login" />
+	const initialState = Object.freeze({ email: "", password: ""})
+	const [credentials, setCredentials] = useState(initialState)
+
+	const handleLogin = (e) => {
+		e.preventDefault()
+
+		setCredentials({...credentials, [e.target.name] : e.target.value.trim()})
+	}
+
+	return (
+		<div className="page-bg">
+			<div className="login-container row">
+				<div className="col-md-7 col-sm-7 design-wrapper">
+					<div className="img-wrapper">
+						<img className="img-responsive" src={LoginImage} alt="Login" />
+					</div>
 				</div>
-			</div>
-			<div className="col-md-3 offset-md-1 form-wrapper">
-				<div className="custom-center">
-					<div className="title">Welcome</div>
-					<img className="img-fluid" src={RyansMedia} alt="Ryans Media" />
-					
-					{
-						[
-							{ title: "Email Address", icon: EmailIcon, name: 'email', placeholder: 'type email...' },
-							{ title: "Password", icon: PasswordIcon, name: 'password', placeholder: 'type password...' }
-						].map(each => (
+				<div className="col-md-4 col-sm-4 offset-md-1 form-wrapper">
+					<div>
+						<h2 className="title">Welcome</h2>
+						<img className="img-fluid" src={RyansMedia} alt="Ryans Media" />
+						
+						<form>
 							<div className="row input-container">
-								<div className="col-2 item-center"> <img className="form-icon" src={each.icon} alt="Icon" /> </div>
+								<div className="col-2 item-center"> <img className="form-icon" src={EmailIcon} alt="Icon" /> </div>
 								<div className="col-9 border-left">
-									<span className="form-title"> {each.title} </span>
-									<input className="input-box" type="text" name={each.name} placeholder={each.placeholder} />
+									<span className="form-title"> Email Address </span>
+									<input 
+										className="input-box" 
+										type="email" 
+										autoComplete='off' 
+										name="email" 
+										placeholder="type email..."
+										onChange={handleLogin} 
+									/>
 								</div>
 							</div>
-						))
-					}
 
-					<div className="col-md-12 btn-wrapper">
-						<button className="login-btn">Login</button>
+							<div className="row input-container">
+								<div className="col-2 item-center"> <img className="form-icon" src={PasswordIcon} alt="Icon" /> </div>
+								<div className="col-9 border-left">
+									<span className="form-title"> Password </span>
+									<input 
+										className="input-box" 
+										type="password" 
+										autoComplete='off' 
+										name="password" 
+										placeholder="type password..."
+										onChange={handleLogin} 
+									/>
+								</div>
+							</div>
+
+							<div className="row align-items-center">
+								<div className="col-md-6">
+									<button className="login-btn">Login</button>
+								</div>
+								<div className="col-md-6 text-center">
+									<span onClick={() => console.log(credentials)}>Forgot Password ?</span>
+								</div>
+							</div>
+						</form>
+						
+						<div className="grow"></div>
+						<div>footer</div>
 					</div>
-					<div className="col-md-6 offset-10 mt-3">Forgot Password ?</div>
 				</div>
-				<div className="grow"></div>
-				<div>footer</div>
 			</div>
 		</div>
-	</div>
-  )
+	)
 }
