a41dd9748e8662537e148b0af1a3852712dabd44
Rev-namdas <sadmanahmedsadman@gmail.com>
2022-04-23 17:15:01 +0600

    Layout Problem

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -25,54 +25,66 @@ export default function LoginPage() {
 						<img className="img-responsive" src={LoginImage} alt="Login" />
 					</div>
 				</div>
-				<div className="col-md-4 col-sm-4 offset-md-1 form-wrapper">
-					<div>
-						<h2 className="title">Welcome</h2>
-						<img className="img-fluid" src={RyansMedia} alt="Ryans Media" />
+				<div className="col-md-4 col-sm-4 offset-md-1">
+					<div className="row border-1 form-wrapper">
+						<form className="display-flex">
+							<div className="border-1 flex-start">
+								<h2 className="title">Welcome</h2>
+								<img className="img-fluid" src={RyansMedia} alt="Ryans Media" />
+							
+								<div className="row input-container">
+									<div className="col-2 item-center"> <img className="form-icon" src={EmailIcon} alt="Icon" /> </div>
+									<div className="col-9 border-left">
+										<span className="form-title"> Email Address </span>
+										<input 
+											className="input-box" 
+											type="email" 
+											autoComplete='off' 
+											name="email" 
+											placeholder="type email..."
+											onChange={handleLogin} 
+										/>
+									</div>
+								</div>
+
+								<div className="row input-container">
+									<div className="col-2 item-center"> <img className="form-icon" src={PasswordIcon} alt="Icon" /> </div>
+									<div className="col-9 border-left">
+										<span className="form-title"> Password </span>
+										<input 
+											className="input-box" 
+											type="password" 
+											autoComplete='off' 
+											name="password" 
+											placeholder="type password..."
+											onChange={handleLogin} 
+										/>
+									</div>
+								</div>
+
+								<div className="row">
+									<div className="col-6">
+										<button className="login-btn">Login</button>
+									</div>
+									<div className="col-6 d-flex align-items-center justify-content-end">
+										<span onClick={() => console.log(credentials)} className="forgotpass">Forgot Password ?</span>
+									</div>
+								</div>
+							</div>
 						
-						<form>
-							<div className="row input-container">
-								<div className="col-2 item-center"> <img className="form-icon" src={EmailIcon} alt="Icon" /> </div>
-								<div className="col-9 border-left">
-									<span className="form-title"> Email Address </span>
-									<input 
-										className="input-box" 
-										type="email" 
-										autoComplete='off' 
-										name="email" 
-										placeholder="type email..."
-										onChange={handleLogin} 
-									/>
-								</div>
-							</div>
+							<div className="flexgrow"></div>
 
-							<div className="row input-container">
-								<div className="col-2 item-center"> <img className="form-icon" src={PasswordIcon} alt="Icon" /> </div>
-								<div className="col-9 border-left">
-									<span className="form-title"> Password </span>
-									<input 
-										className="input-box" 
-										type="password" 
-										autoComplete='off' 
-										name="password" 
-										placeholder="type password..."
-										onChange={handleLogin} 
-									/>
+							<div className="row border-1">
+								<div className="col-6">
+									<div className="row">www.ryansarchives.com</div>
+									<div className="row">icon</div>
 								</div>
-							</div>
-
-							<div className="row align-items-center">
-								<div className="col-md-6">
-									<button className="login-btn">Login</button>
-								</div>
-								<div className="col-md-6 text-center">
-									<span onClick={() => console.log(credentials)}>Forgot Password ?</span>
+								<div className="col-6">
+									<div className="row">Visit Our Social Sites:</div>
+									<div className="row">icon</div>
 								</div>
 							</div>
 						</form>
-						
-						<div className="grow"></div>
-						<div>footer</div>
 					</div>
 				</div>
 			</div>
